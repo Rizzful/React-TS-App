b975695f8bdb92b32d111e9eca8beafbcac8c565
Fllynn <jurobrod@gmail.com>
2023-09-16 00:27:28 +0200

    Base App

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
+
+const rootElement = document.getElementById("root");
+
+const root = createRoot(rootElement!);
+
+root.render(
+    <App />
+);
